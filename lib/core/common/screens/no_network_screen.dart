dfb7bb4b9eaf5ce0b9eeaf58829b3df1510d2c87
yusufabdo20 <yusufa.fattah@gmail.com>
2024-07-14 14:31:27 +0300

    Add no network screen and app images
    Added new images for the app's core functionality, including empty screen, no network, page under build, and splash. Also added a new screen for no network and updated the pubspec.yaml file to include the new image assets.

---
+++ lib/core/common/screens/no_network_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ystore/core/style/app_images.dart';
+
+class NoNetworkScreen extends StatelessWidget {
+  const NoNetworkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AppImages.noNetwork),
+              fit: BoxFit.fill,
+             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
